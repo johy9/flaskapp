352fbb76bd924bc7f065da401f3ecc3392420bac
johy9 <joysiki93@gmail.com>
2023-03-30 00:12:49 -0400

    up

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
+#!/bin/python
+import logging
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
- return "I am almost a Devops Engineer!"
+def index():
+    return "I am almost a Devops Engineer!"
+
 if __name__ == "__main__":
- app.run(host='0.0.0.0')
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
+    logging.info('Starting Flask app...')
+    app.run(host='0.0.0.0')
+    logging.info('Flask app has been started')
