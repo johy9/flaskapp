36aa996118c449aabe0956ae674809a22723abee
johy9 <124749142+johy9@users.noreply.github.com>
2023-04-01 12:14:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello there! I'm a DevOps Engineer in the making, and I'm excited to use Flask to build some awesome web applications. Stay tuned for some amazing projects coming soon! :)"
+    return "Hello there! I'm a DevOps Engineer in the making, and I'm excited to use Flask to build some awesome web applications. Stay tuned for some amazing projects coming soon!"
 
 if __name__ == "__main__":
  app.run(host='0.0.0.0')
