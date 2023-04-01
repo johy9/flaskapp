6cfea8c998a492c006fa70d5288572fd380b2d1c
johy9 <124749142+johy9@users.noreply.github.com>
2023-03-31 22:36:21 -0400

    Create tests.py

---
+++ tests.py
@@ -0,0 +1,14 @@
+import unittest
+from app import app
+
+class TestApp(unittest.TestCase):
+    
+    def test_index(self):
+        tester = app.test_client(self)
+        response = tester.get('/')
+        status_code = response.status_code
+        self.assertEqual(status_code, 200)
+        self.assertIn(b 'I am almost a Devops Engineer!', response.data)
+
+if __name__ == '__main__':
+    unittest.main()
